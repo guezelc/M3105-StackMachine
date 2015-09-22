dc9fb4fcb0715dfb2785cff9475872f27df7bbf4
GUEZEL Clement <guezelc@PC-B103-07.iut-valence.fr>
2015-09-22 16:55:35 +0200

    the application compiles exepct for  : opCode

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/CPU.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/CPU.java
@@ -27,6 +27,7 @@ public class CPU
 	public final static int JZ		= 0x13;
 	public final static int DUP		= 0x14;
 	public final static int POP		= 0x15;
+	private IO ioSystem;
 	
 	// TODO something is missing here...
 
@@ -162,7 +163,7 @@ public class CPU
 		}
 		catch (AddressOutOfBoundsException e)
 		{
-			// TODO something is missing here...
+			
 		}
 		catch (IOException e)
 		{
@@ -176,6 +177,36 @@ public class CPU
 		{
 			// TODO something is missing here...
 		}		
+	}
+
+	public void wireToProgramMemory(Memory programMemory) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void wireToCallStack(Stack callStack) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void wireToExpStack(Stack expStack) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void wireToIoSubsystem(IO ioSystem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void clearStacks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPC(int address) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	// TODO something is missing here...
